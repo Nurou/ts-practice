d196a9b9cca33ef4bf0982f1cf5acef6ef0c527c
Joel Hassan <joel.nhn@gmail.com>
2020-08-30 14:52:35 +0300

    9.15

--- react-with-types/src/index.tsx
+++ react-with-types/src/index.tsx
@@ -1,56 +1,133 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { assert } from 'console';
 
-interface ICoursePart {
+// new types
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
 
-interface ICourseParts {
-  courseParts: ICoursePart[];
+interface CoursePartBaseExtended extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartBaseExtended {
+  name: 'Fundamentals';
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: 'Using props to pass data';
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBaseExtended {
+  name: 'Deeper type usage';
+  exerciseSubmissionLink: string;
+}
+interface CoursePartCustom extends CoursePartBaseExtended {
+  name: 'Custom part';
+}
+
+type CoursePart =
+  | CoursePartOne
+  | CoursePartTwo
+  | CoursePartThree
+  | CoursePartCustom;
+
+interface CourseParts {
+  courseParts: CoursePart[];
 }
 
 type TCourseName = { courseName: string };
+
+/**
+ * Helper function for exhaustive type checking
+ */
+const exhaustiveCheck = (coursePartProperty: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(
+      coursePartProperty
+    )}`
+  );
+};
 
 const Header = ({ courseName }: TCourseName) => {
   return <h1>{courseName}</h1>;
 };
 
-const Content = ({ courseParts }: ICourseParts) => {
+const Part: React.FC<{ coursePart: CoursePart }> = ({ coursePart }) => {
   return (
     <>
-      {courseParts.map((part: ICoursePart) => (
-        <p>{part.name}</p>
+      {Object.keys(coursePart).map((k, i) => (
+        <p>
+          {k}: {Object.values(coursePart)[i]}
+        </p>
       ))}
     </>
   );
 };
 
-const Total = ({ courseParts }: ICourseParts) => {
+const Content = ({ courseParts }: CourseParts) => {
+  courseParts.forEach((part) => {
+    switch (part.name) {
+      case 'Fundamentals':
+        break;
+      case 'Using props to pass data':
+        break;
+      case 'Deeper type usage':
+        break;
+      case 'Custom part':
+        break;
+      default:
+        return exhaustiveCheck(part);
+    }
+  });
+
+  return (
+    <>
+      {courseParts.map((part: CoursePart) => (
+        <Part coursePart={part} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ courseParts }: CourseParts) => {
   return (
     <p>
       Number of exercises{' '}
       {courseParts.reduce(
-        (carry: number, part: ICoursePart) => carry + part.exerciseCount,
+        (carry: number, part: CoursePart) => carry + part.exerciseCount,
         0
       )}
     </p>
   );
 };
+
 const App: React.FC = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
+      description: 'This is an awesome course part',
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
+      groupProjectCount: 3,
     },
     {
       name: 'Deeper type usage',
       exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+    },
+    {
+      name: 'Custom part',
+      exerciseCount: 10,
+      description: 'Testing...',
     },
   ];
 
